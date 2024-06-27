f55a1553998b6248acf664e942e26208747a46e1
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-06-27 15:00:27 +0900

    mongoDB 설치

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express')
 const app = express()
 const port = 4000
 
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://bestsaejin12:7th7r2dSoWLl2BBS@boilerplate.xixcwut.mongodb.net/?retryWrites=true&w=majority&appName=boilerplate', {
+}).then(() => console.log('MongoDB Connected...'))
+.catch(err => console.log(err))
+
 app.get('/', (req, res) => {  res.send('Hello World!')})
-""
+
 app.listen(port, () => {  console.log(`Example app listening on port ${port}`)})
